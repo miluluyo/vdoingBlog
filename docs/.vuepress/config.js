97681d500f88d6f518b26837ea1b489e3b02e6d5
miluluyo@163.com <miluluyo@163.com>
2020-07-17 14:16:59 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const nav = require('./config/nav.js');
 module.exports = {
   title: "麋鹿鲁哟",
   description: '运气交给锦鲤，你只管努力就行。', // 描述,以 <meta> 标签渲染到页面html中
-  base: '/miluluyo.github.io/vdoingBlog/', // '/<github仓库名>/'， 默认'/' 
+  base: '/vdoingBlog/', // '/<github仓库名>/'， 默认'/' 
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/milu.icon' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
