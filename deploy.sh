49932bf1fd600285ce8388ad882846b153405070
miluluyo@163.com <miluluyo@163.com>
2020-07-21 09:33:24 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -11,20 +11,22 @@ cd docs/.vuepress/dist
 
 # deploy to github
 #echo 'b.miluluyo.github.io' > CNAME
-#if [ -z "$VDOING" ]; then
-  #msg='deploy'
-  #githubUrl=git@github.com:miluluyo/vdoingBlog.git
-#else
-  #msg='来自 github actions的自动部署'
-  #githubUrl=https://miluluyo:${VDOING}@github.com/miluluyo/vdoingBlog.git
-  #git config --global user.name "miluluyo"
- # git config --global user.email "miluluyo@163.com"
-#fi
+if [ -z "$VDOING" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:miluluyo/vdoingBlog.git
+else
+  msg='来自 github actions的自动部署'
+  githubUrl=https://miluluyo:${VDOING}@github.com/miluluyo/vdoingBlog.git
+  git config --global user.name "miluluyo"
+  git config --global user.email "miluluyo@163.com"
+fi
 git init
 git add -A
 git commit -m "deploy"
 #git push -f $githubUrl master:gh-pages # 推送到github
 git push -f git@github.com:miluluyo/vdoingBlog.git master:gh-pages
+
+
 # deploy to coding
 #echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
 #if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
