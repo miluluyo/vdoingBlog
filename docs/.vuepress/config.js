8c799d9c6b707c895e829437f2f7e2f55bbd5343
麋鹿鲁哟 <38480924+miluluyo@users.noreply.github.com>
2023-03-31 14:56:41 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const htmlModules = require('./config/htmlModules.js');
 module.exports = {
   title: "麋鹿鲁哟",
   description: '运气交给锦鲤，你只管努力就行。', // 描述,以 <meta> 标签渲染到页面html中
-  base: '/vdoingBlog/', // '/<github仓库名>/'， 默认'/' 
+  base: '', // '/<github仓库名>/'， 默认'/' 
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/milu.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'}],
